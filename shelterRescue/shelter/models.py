fd23f0ebcb9282f110c25e89ef3281aa9400b01f
John Jascovski <elejaski@gmail.com>
2017-09-16 19:49:26 -0700

    Models and Django admin

--- shelterRescue/shelter/models.py
+++ shelterRescue/shelter/models.py
@@ -2,5 +2,32 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Disaster(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=32)
+    
+
+class Shelter(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    disaster = models.ForeignKey(Disaster, null=True, blank=True)
+    name = models.CharField(max_length=32)
+    status = models.CharField(max_length=32)
+    location = models.CharField(max_length=32)
+    capacity = models.IntegerField(null=True, blank=True)
+
+
+class Person(models.Model):
+	
+    id = models.AutoField(primary_key=True)
+    shelter = models.ForeignKey(Shelter, null=True, blank=True)
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32)
+    emergencyName = models.CharField(max_length=32)
+    emergencyPhone = models.CharField(max_length=32)
+    emergencyEmail = models.CharField(max_length=32)
